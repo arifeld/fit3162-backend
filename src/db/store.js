78aa076a959cc99636dd6050e8cf2073154c2250
arifeld <24385099+arifeld@users.noreply.github.com>
2024-08-18 20:53:52 +1000

    Fix get single store not filtering correctly

--- src/db/store.js
+++ src/db/store.js
@@ -29,17 +29,16 @@ const getStore = function(db, id, callback) {
     const getStoreQuery = `
         SELECT store.store_id, store_name, store_description, store_address_street, store_address_suburb, store_address_postcode, store_geopoint, store_contact_phone, store_contact_email, store_contact_website, business_id, JSON_ARRAYAGG(JSON_OBJECT("name", category.category_name, "description", category.category_description)) AS categories 
         FROM store
-        LEFT JOIN store_category ON store.store_id = store_category.store_id AND store.store_id = ?
+        LEFT JOIN store_category ON store.store_id = store_category.store_id
         LEFT JOIN category ON store_category.category_id = category.category_id
+        WHERE store.store_id = ?
         GROUP BY store.store_id;
     `;
-
 
     // No SQL injection vulnerability by using prepared statements
     // The query is also cached thanks to mysql2
     db.execute(getStoreQuery, [id], (err, rows) => {
         if (err) { logger.error(err); return; }
-
         if (callback) { callback(rows); }
     })
 
