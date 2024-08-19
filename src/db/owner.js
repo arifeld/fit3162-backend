e792c24ea52f22835f22e30363361c5af6ad5c94
Suryadeep Singh <suryadeep.singh098@gmail.com>
2024-08-19 16:07:15 +1000

    fixed encryption for passwords

--- src/db/owner.js
+++ src/db/owner.js
@@ -2,33 +2,33 @@ const logger = require("./../logging/logging");
 const bcrypt = require('bcrypt');
 
 
-const setOwner = function(db, owner_id, owner_email, owner_password, callback){
-
+const setOwner = function(db, owner_id, owner_email, owner_password, callback) {
+    // Hash the password here:
     bcrypt.hash(owner_password, 10, (error, hashpassword) => {
-        if(error){
-            console.log(error);
+        if (error) {
+            console.log("Error hashing password", error);
+            return;
         }
-    })
 
-    // we will need to create a query that creates a business:
-    const setBusinessScript = `
-        INSERT INTO Owner (owner_id, owner_email, owner_password) 
-        VALUES (?, ?, ?)`;
+        // SQL query to insert the owner into the database
+        const setBusinessScript = `INSERT INTO Owner (owner_id, owner_email, owner_password) VALUES (?, ?, ?)`;
 
-    const callback_values = [owner_id, owner_email, owner_password];
+        // Use the hashed password instead of the plain text password
+        const callback_values = [owner_id, owner_email, hashpassword];
 
-    db.execute(setBusinessScript, callback_values, (err, result) => {
-        if (err) {
-            console.log("Problem with inserting owner", err);
-            if (callback) {
-                callback(null);
+        // Executing the query
+        db.execute(setBusinessScript, callback_values, (err, result) => {
+            if (err) {
+                console.log("Problem with inserting owner", err);
+                if (callback) {
+                    callback(null);
+                }
+            } else {
+                if (callback) {
+                    callback(result);
+                }
             }
-        } else {
-            if (callback) {
-                callback(result);
-            }
-        }
+        });
     });
-}
-
+};
 module.exports = {setOwner};
