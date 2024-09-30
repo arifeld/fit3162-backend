89cb5f471b42280836807545ab34096b108515de
Ricky Z <rickyzhang2013@hotmail.com>
2024-09-30 15:24:06 +1000

    business endpoints, setBusiness in db auto generates business_id

--- src/db/business.js
+++ src/db/business.js
@@ -16,14 +16,14 @@ const getBusiness = function(db, business_id, callback){
     })
 };
 
-const setBusiness = function(db, business_id, business_name, business_contact_email, business_contact_phone, owner_id, callback){
+const setBusiness = function(db, business_name, business_contact_email, business_contact_phone, owner_id, callback){
 
     // we will need to create a query that creates a business:
     const setBusinessScript = `
-        INSERT INTO business (business_id, business_name, business_contact_email, business_contact_phone, owner_id) 
-        VALUES (?, ?, ?, ?, ?)`;
+        INSERT INTO business (business_name, business_contact_email, business_contact_phone, owner_id) 
+        VALUES (?, ?, ?, ?)`;
 
-    const callback_values = [business_id, business_name, business_contact_email, business_contact_phone, owner_id];
+    const callback_values = [business_name, business_contact_email, business_contact_phone, owner_id];
 
     db.execute(setBusinessScript, callback_values, (err, result) => {
         if (err) {
@@ -39,4 +39,23 @@ const setBusiness = function(db, business_id, business_name, business_contact_em
     });
 }
 
-module.exports = {setBusiness, getBusiness};
+const deleteBusiness = function(db, business_id, callback) {
+    const deleteBusinessScript = ` DELETE FROM business WHERE business_id = ?`;
+
+    db.execute(deleteBusinessScript, [business_id], (err, result) => {
+        if (err) {
+            console.log("Error deleting business", err);
+            if (callback) {
+                callback(null);
+            }
+        }
+        else {
+            if (callback) {
+                callback(result);
+            }
+        }
+    });
+}
+
+
+module.exports = {setBusiness, getBusiness, deleteBusiness};
